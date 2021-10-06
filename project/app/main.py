e42affce6516e8f84f8729b51d0e29c4b6307a9a
MahshadShirzaei <mahshad.shirzaee@onefootball.com>
2021-10-06 17:44:30 +0200

    App structure

--- project/app/main.py
+++ project/app/main.py
@@ -1,25 +1,27 @@
 import os
+import logging
 
-from fastapi import FastAPI, Depends
-from app.config import get_settings, Settings
-from tortoise.contrib.fastapi import register_tortoise
+from fastapi import FastAPI
+from app.api import ping
+from app.db import init_db
+
+log = logging.getLogger("uvicorn")
 
 
-app = FastAPI()
+def create_application() -> FastAPI:
+    application = FastAPI()  
+    application.include_router(ping.router)
 
-# Register Tortoise ORM with DB
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
+    return application
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-        }
+app = create_application()
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event() -> None:
+    log.info("Shutting down...")
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -5,6 +5,8 @@ from starlette.testclient import TestClient
 
 from app import main
 from app.config import get_settings, Settings
+from app.main import create_application
+
 
 def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
@@ -12,8 +14,9 @@ def get_settings_override():
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
 
         # testing
         yield test_client
